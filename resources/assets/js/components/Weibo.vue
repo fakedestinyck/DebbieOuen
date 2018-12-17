3c80b5578f3c9bbcbe9fab099d2b7b3811303a41
Haochang Qian <fakedestinyck@gmail.com>
2018-12-17 18:40:17 -0500

    fix 15 days

--- resources/assets/js/components/Weibo.vue
+++ resources/assets/js/components/Weibo.vue
@@ -228,22 +228,7 @@
                     //
                 })
                     .then(function (response) {
-                        let data = response.data;
-//                        console.log(response);
-                        if (data === "") {
-                            that.thirtyDaysTweetsText = "没有更新的了";
-                            that.thirtyDaysTweetsId = null;
-                            that.$refs.thirtyDaysDayXInput.style.display = 'none';
-                            return;
-                        }
-                        that.thirtyDaysTweetsText = data.text.replace(/\n/g, "<br>");
-                        that.thirtyDaysTweetsDate = data.tweets_time;
-                        that.thirtyDaysTweetsId = data.id;
-                        that.thirtyDaysTweetsTweetId = data.tweets_id;
-                        that.loadingNextTweets = false;
-                        setTimeout(function(){
-                            that.$refs.thirtyDaysDayXInput.focus();
-                        }, 1);
+
                     })
                     .catch(function (error) {
                         alert(error);
