a788b0d50bb566d714e8ced2bdca071747af930f
Haochang Qian <fakedestinyck@gmail.com>
2018-12-31 00:15:55 +0800

    Add 30days anli reminder count

--- app/Http/Controllers/WeiboController.php
+++ app/Http/Controllers/WeiboController.php
@@ -249,10 +249,52 @@ class WeiboController extends Controller
         }
 
 
-        if ($endDay == 15) {
-            foreach ($completeness[$endDay] as $user_array) {
+        if ($endDay == 29) {
+            echo ($endDay-1)."天：";
+            foreach ($completeness[$endDay-1] as $user_array) {
                 echo $user_array["username"] . "，";
             }
+            echo "<br>";
+            echo ($endDay-2)."天：";
+            foreach ($completeness[$endDay-2] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-3)."天：";
+            foreach ($completeness[$endDay-3] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-4)."天：";
+            foreach ($completeness[$endDay-4] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-5)."天：";
+            foreach ($completeness[$endDay-5] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-6)."天：";
+            foreach ($completeness[$endDay-6] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-7)."天：";
+            foreach ($completeness[$endDay-7] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-8)."天：";
+            foreach ($completeness[$endDay-8] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
+            echo ($endDay-9)."天：";
+            foreach ($completeness[$endDay-9] as $user_array) {
+                echo $user_array["username"] . "，";
+            }
+            echo "<br>";
         } else {
             return $completeness;
         }
