b8983a01f104e6b065b50775dee8cb257040a4a3
Haochang Qian <fakedestinyck@gmail.com>
2019-01-27 12:48:43 -0500

    Change link valid time to 1 minute

--- app/Http/Controllers/SmurfController.php
+++ app/Http/Controllers/SmurfController.php
@@ -124,7 +124,7 @@ class SmurfController extends Controller
 
         $nowtime = time();
         $delta_time= $nowtime - $timestamp;
-        if ($delta_time > 300 || $delta_time < 0) {
+        if ($delta_time > 60 || $delta_time < 0) {
             return "链接已经过期，请重新向机器人获取链接";
         }
 
