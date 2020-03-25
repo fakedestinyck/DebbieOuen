a8914d69cb98baac12697201d29d3d9af905c136
Caspar Qian <fakedestinyck@gmail.com>
2020-03-25 11:43:32 +0800

    Add tables for taoba

--- database/migrations/2020_03_25_072341_create_orders_table.php
+++ database/migrations/2020_03_25_072341_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectsTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,13 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedTinyInteger('platform');
             $table->unsignedInteger('pro_id');
-            $table->string('title');
-            $table->string('start_time');
-            $table->string('end_time')->nullable();
+            $table->unsignedInteger('user_id');
             $table->double('amount');
-            $table->unsignedInteger('order_num');
-            $table->string('other_info');
-            $table->double('target');
+            $table->string('signature');
             $table->timestamps();
         });
     }
@@ -35,6 +31,6 @@ class CreateProjectsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('orders');
     }
 }
